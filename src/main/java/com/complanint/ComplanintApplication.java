99664b205996584c9d7f7839b9a250a70de99af2
osy01236 <cksdyd54321@gmail.com>
2026-03-04 17:03:48 +0900

    123456789

---
+++ src/main/java/com/complanint/ComplanintApplication.java
@@ -0,0 +1,13 @@
+package com.complanint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplanintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplanintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/complanint/ComplanintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complanint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplanintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
